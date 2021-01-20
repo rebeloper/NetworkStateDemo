be3a869faee95ab51c415efaf652dd49832adb7d
Alex Nagy <therebeloper@gmail.com>
2021-01-20 18:34:58 +0200

    Initial Commit

---
+++ NetworkStateDemo/NetworkStateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkStateDemoApp.swift
+//  NetworkStateDemo
+//
+//  Created by Alex Nagy on 20.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct NetworkStateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
